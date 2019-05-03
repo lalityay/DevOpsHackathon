97e095d34529f5442433833cc4fce053bfdf77b8
Obulasetty, Rakesh <rakesh_obulasetty@spglobal.com>
2019-05-03 21:17:58 +0530

    added file logging

--- WeatherApp/Startup.cs
+++ WeatherApp/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace WeatherApp
 {
@@ -12,6 +14,8 @@ namespace WeatherApp
     {
         public Startup(IConfiguration configuration)
         {
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
             Configuration = configuration;
         }
 
@@ -20,6 +24,10 @@ namespace WeatherApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
@@ -30,8 +38,15 @@ namespace WeatherApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddSerilog();
+
+            var logger=loggerFactory.CreateLogger("testlogger");
+
+            logger.LogTrace("test");
+
+            logger.LogTrace("test");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
